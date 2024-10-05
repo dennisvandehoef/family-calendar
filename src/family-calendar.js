35bd0207fc55ac664da17c6140e88dfe11bb6a3c
Dennis van de Hoef <dennisvdhoef@gmail.com>
2024-10-05 14:25:16 +0200

    Fix showing more than one event per calendar

--- src/family-calendar.js
+++ src/family-calendar.js
@@ -7,6 +7,15 @@ export class FamilyCalendarCard extends LitElement {
 
     set hass(hass) {
         this._hass = hass;
+        if (!this._eventsFetched) {
+            this._fetchEvents();
+        }
+    }
+
+    constructor() {
+        super();
+        this.events = {}; // Contains events for each calendar
+        this._eventsFetched = false;
     }
 
     render() {
@@ -53,52 +62,97 @@ export class FamilyCalendarCard extends LitElement {
         const dateString = date.toFormat('yyyy-MM-dd');
 
         return html`
-        <div class="family-calendar--row">
-            <div class="family-calendar--field family-calendar--row-header">${date.toLocaleString(DateTime.DATE_MED)}</div>
-            ${this.config.columns.map((column) => {
-            const eventsForColumn = this._getEventsForColumn(dateString, column);
+            <div class="family-calendar--row">
+                <div class="family-calendar--field family-calendar--row-header">${date.toLocaleString(DateTime.DATE_MED)}</div>
+                ${this.config.columns.map((column) => {
+            const eventsForColumn = this._getEventsForColumn(dateString, column.calendars);
             return html`
-                    <div class="family-calendar--field family-calendar--date">
-                        ${eventsForColumn.length > 0
+                        <div class="family-calendar--field family-calendar--date">
+                            ${eventsForColumn.length > 0
                     ? eventsForColumn.map(event => html`
-                                <div class="family-calendar--event">
-                                    <div class="family-calendar--event-time">${event.time}</div>
-                                    <div class="family-calendar--event-title">${event.title}</div>
-                                </div>
-                            `)
+                                    <div class="family-calendar--event">
+                                        <div class="family-calendar--event-time">${event.time}</div>
+                                        <div class="family-calendar--event-title">${event.title}</div>
+                                    </div>
+                                `)
                     : 'No events'}
-                    </div>
-                `;
+                        </div>
+                    `;
         })}
-        </div>
-    `;
+            </div>
+        `;
     }
 
-    _getEventsForColumn(dateString, column) {
-        return column.calendars.map((calendar) => {
-            const stateObj = this._hass.states[calendar];
-            if (!stateObj || !stateObj.attributes.start_time || !stateObj.attributes.message) {
-                return [];
+    _getEventsForColumn(dateString, calendars) {
+        const eventsForColumn = [];
+
+        // Loop through each calendar in the column
+        calendars.forEach(calendar => {
+            if (this.events[calendar]) {
+                this.events[calendar].forEach(event => {
+                    const eventStart = DateTime.fromISO(event.start);
+                    const eventEnd = DateTime.fromISO(event.end);
+
+                    // Check if the event date matches the row's date or spans over this date
+                    if (eventStart.toFormat('yyyy-MM-dd') === dateString || (eventStart < DateTime.fromISO(dateString) && eventEnd >= DateTime.fromISO(dateString))) {
+                        const isFullDayEvent = eventStart.hasSame(eventEnd, 'day') && event.all_day;
+
+                        const timeRange = isFullDayEvent
+                            ? 'All Day'
+                            : `${eventStart.toLocaleString(DateTime.TIME_SIMPLE)} - ${eventEnd.toLocaleString(DateTime.TIME_SIMPLE)}`;
+
+                        eventsForColumn.push({
+                            time: timeRange,
+                            title: event.summary,
+                        });
+                    }
+                });
             }
+        });
 
-            // Convert to ISO format
-            const startTime = stateObj.attributes.start_time.replace(' ', 'T'); // Converts '2024-09-30 15:00:00' to '2024-09-30T15:00:00'
-            const eventStart = DateTime.fromISO(startTime);
+        return eventsForColumn;
+    }
 
-            // Check if the event date matches the date of the row
-            if (eventStart.toFormat('yyyy-MM-dd') === dateString) {
-                const endTime = stateObj.attributes.end_time ? stateObj.attributes.end_time.replace(' ', 'T') : null;
-                const timeRange = endTime
-                    ? `${eventStart.toLocaleString(DateTime.TIME_SIMPLE)} - ${DateTime.fromISO(endTime).toLocaleString(DateTime.TIME_SIMPLE)}`
-                    : `${eventStart.toLocaleString(DateTime.TIME_SIMPLE)}`;
+    async _fetchEvents() {
+        let currentDate = DateTime.now();
+        let endDate = currentDate.plus({ days: 30 });
 
-                return {
-                    time: timeRange,
-                    title: stateObj.attributes.message
-                };
-            }
-            return [];
-        }).flat(); // Flatten the array
+        let startDateISO = currentDate.toISO();
+        let endDateISO = endDate.toISO();
+
+        // Clear all previous events before fetching new ones
+        this.events = {};
+
+        this.config.columns.forEach(column => {
+            column.calendars.forEach(calendar => {
+                // Reset events for each calendar before fetching
+                this.events[calendar] = [];
+
+                this._hass.callApi(
+                    'get',
+                    `calendars/${calendar}?start=${encodeURIComponent(startDateISO)}&end=${encodeURIComponent(endDateISO)}`
+                ).then(response => {
+                    response.forEach(event => {
+                        const startDate = DateTime.fromISO(event.start.dateTime);
+                        const endDate = DateTime.fromISO(event.end.dateTime);
+
+                        let fullDay = !event.start.dateTime;
+
+                        this.events[calendar].push({
+                            summary: event.summary,
+                            start: startDate.toISO(),
+                            end: endDate.toISO(),
+                            all_day: fullDay,
+                        });
+                    });
+
+                    this._eventsFetched = true;
+                    this.requestUpdate(); // Re-render after events are fetched
+                }).catch(error => {
+                    console.error('Error while fetching calendar:', error);
+                });
+            });
+        });
     }
 
     setConfig(config) {
